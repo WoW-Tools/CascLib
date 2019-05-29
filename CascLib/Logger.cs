975506806453596923d9cddf61cb36051fdfa54f
TOM_RUS <tomrus88@github.com>
2019-05-29 05:07:03 +0300

    Fix

--- CascLib/Logger.cs
+++ CascLib/Logger.cs
@@ -6,36 +6,41 @@ namespace CASCLib
     public interface ILoggerOptions
     {
         string LogFileName { get; }
+        bool TimeStamp { get; }
     }
 
     public class LoggerOptionsDefault : ILoggerOptions
     {
         public string LogFileName => "debug.log";
+        public bool TimeStamp => true;
     }
 
     public class LoggerOptionsFileNameWithDate : ILoggerOptions
     {
         public string LogFileName => $"debug-{DateTime.Now:yyyyMMdd-HHmmss}.log";
+        public bool TimeStamp => true;
     }
 
     public class Logger
     {
         private static FileStream fs;
         private static StreamWriter logger;
+        private static ILoggerOptions options;
 
         public static void Init(ILoggerOptions opts = null)
         {
-            opts = opts ?? new LoggerOptionsDefault();
-            fs = new FileStream(opts.LogFileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            options = opts ?? new LoggerOptionsDefault();
+            fs = new FileStream(options.LogFileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
             logger = new StreamWriter(fs) { AutoFlush = true };
         }
 
         public static void WriteLine(string format, params object[] args)
         {
-            if (fs == null || logger == null)
+            if (options == null || fs == null || logger == null)
                 throw new InvalidOperationException("Logger isn't initialized!");
 
-            logger.Write("[{0}]: ", DateTime.Now);
+            if (options.TimeStamp)
+                logger.Write($"[{DateTime.Now}]: ");
             logger.WriteLine(format, args);
         }
     }
