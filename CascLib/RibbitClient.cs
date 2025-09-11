748cd50c85829a8b1d0897728db0b19a6fdd76cd
TOM_RUS <tomrus88@gmail.com>
2025-09-11 15:02:25 +0300

    Fix

--- CascLib/RibbitClient.cs
+++ CascLib/RibbitClient.cs
@@ -1,6 +1,8 @@
 ﻿using MimeKit;
 using System;
 using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
 
@@ -15,26 +17,55 @@ namespace CASCLib
     {
         private const string ribbitHost = ".version.battle.net";
         private readonly int version;
-        private readonly TcpClient client = new TcpClient();
+        private readonly string region;
+        private readonly TcpClient client;
 
-        public RibbitClient(string region, int version = 1)
+        public RibbitClient(string region, int version = 2)
         {
-            client = new TcpClient(region + ribbitHost, 1119);
+            this.region = region ?? throw new ArgumentNullException(nameof(region));
             this.version = version;
+
+            if (version == 1)
+                client = new TcpClient(region + ribbitHost, 1119);
         }
 
         public string Get(string request)
         {
-            using (NetworkStream stream = client.GetStream())
+            switch (version)
             {
-                byte[] req = Encoding.ASCII.GetBytes(request + "\r\n");
+                case 1:
+                    using (var stream = client.GetStream())
+                    {
+                        byte[] req = Encoding.ASCII.GetBytes(request + "\r\n");
 
-                stream.Write(req, 0, req.Length);
+                        stream.Write(req, 0, req.Length);
 
-                var message = MimeMessage.Load(stream);
+                        var message = MimeMessage.Load(stream);
 
-                return message.TextBody;
+                        return message.TextBody;
+                    }
+                case 2:
+                    return GetV2Https(request);
+                default:
+                    throw new InvalidOperationException($"unsupported protocol version {version}");
             }
+        }
+
+        private string GetV2Https(string request)
+        {
+            // v2 lives at: https://{region}.version.battle.net/{request}
+            var url = $"https://{region}{ribbitHost}/{request}";
+
+            using var handler = new HttpClientHandler();
+            handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            using var http = new HttpClient(handler);
+            http.Timeout = TimeSpan.FromSeconds(30);
+
+            http.DefaultRequestHeaders.UserAgent.ParseAdd("CASCLib.RibbitClient/2");
+
+            var resp = http.GetAsync(url).GetAwaiter().GetResult();
+            resp.EnsureSuccessStatusCode();
+            return resp.Content.ReadAsStringAsync().GetAwaiter().GetResult();
         }
 
         private string MakeRequestString(string product, ProductInfoType infoType)
@@ -65,7 +96,7 @@ namespace CASCLib
 
         public void Dispose()
         {
-            client.Dispose();
+            client?.Dispose();
         }
     }
 }
